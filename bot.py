7d4b054784cc27ed97c8fc575fbdf4a358ed5913
maryszmary <masha.shejanova@gmail.com>
2021-06-25 01:15:22 +0300

    fix for start_webhook

--- bot.py
+++ bot.py
@@ -119,10 +119,11 @@ def main() -> None:
 
 
 def run_webhook():
-    UPDATER.start_webhook(listen="0.0.0.0",
-                        port=PORT,
-                        url_path=TOKEN)
-    UPDATER.bot.set_webhook(os.environ.get('MY_URL') + '/' + TOKEN)
+    UPDATER.start_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        url_path=TOKEN,
+        webhook_url=os.environ.get('MY_URL', 'localhost') + '/' + TOKEN)
     UPDATER.idle()
 
 if __name__ == '__main__':
